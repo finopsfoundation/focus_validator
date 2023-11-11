a91b70eb9b08438d397159ef541699b799c9b091
Joshua Kwan <joshua.m.kwan@gmail.com>
2023-11-11 09:28:16 -0500

    fix poetry build not including version_sets/0.5 files (#97)
    Signed-off-by: Joshua Kwan <joshk@ternary.app>

--- tests/test_match_check_id_rule_config_file.py
+++ tests/test_match_check_id_rule_config_file.py
@@ -15,16 +15,3 @@ class TestMatchCheckIdRuleConfigFile(TestCase):
                 rule = Rule.load_yaml(rule_path=rule_path)
                 self.assertIsInstance(rule, Rule)
                 self.assertEqual(rule.check_id, Path(name).stem)
-
-    def test_version_sets(self):
-        for root, dirs, files in os.walk(
-            "focus_validator/rules/version_sets", topdown=False
-        ):
-            for name in files:
-                rule_path = os.path.join(root, name)
-                self.assertTrue(
-                    os.path.islink(rule_path), f"path not a sym link, {rule_path}"
-                )
-                self.assertTrue(
-                    Path(rule_path).exists(), f"invalid sym link, {rule_path}"
-                )
