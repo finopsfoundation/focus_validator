c58c2b011e60dc40cd8d4cafe0110f9aaff7b33e
johrenberger <122705393+johrenberger@users.noreply.github.com>
2024-01-13 08:38:45 -0600

    New performance profiler and updates to test data generation (#110)
    Signed-off-by: Justin Ohrenberger <justinohrenberger@gmail.com>

--- tests/samples/csv_random_data_generate_at_scale.py
+++ tests/samples/csv_random_data_generate_at_scale.py
@@ -1,19 +1,29 @@
-from concurrent.futures import ThreadPoolExecutor
-import csv
 import functools
-import io
-import logging
-import random
-import time
-import pytz
-from datetime import datetime, timedelta
+import polars as pl
 from faker import Faker
+import random
+from datetime import datetime, timedelta
+import pytz
+import logging
+import time
 
 fake = Faker()
 
+def get_aws_invoice_issuer(num_records):
+    aws_entities = [
+        'AWS Inc.', 'Amazon Web Services', 'AWS Marketplace', 
+        'Amazon Data Services', 'AWS CloudFront', 'Amazon S3 Billing', 
+        'Amazon EC2 Billing', 'AWS Lambda Billing'
+    ]
+    return [random.choice(aws_entities) for _ in range(num_records)]
+
+# ... similar functions for other non-date attributes ...
+
+def get_random_datetimes(num_records, start_date, end_date):
+    return [fake.date_time_between(start_date=start_date, end_date=end_date, tzinfo=pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ') for _ in range(num_records)]
+
 def log_execution_time(func):
     """Decorator to log the execution time of a function."""
-
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.time()
@@ -21,230 +31,50 @@ def log_execution_time(func):
         end_time = time.time()
         logging.info(f"{func.__name__} executed in {end_time - start_time:.2f} seconds")
         return result
-
     return wrapper
-
-class FakeFocus:
-    
-    def __init__(self):
-        self._cache = {}
-    
-    # Current time in UTC
-    now = datetime.now(pytz.utc)
-
-    # 30 days ago from now
-    thirty_days_ago = now - timedelta(days=30)
-
-    @property
-    def InvoiceIssuer(self):
-        return self._cached_property('InvoiceIssuer', self.get_aws_invoice_issuer)
-
-    @property
-    def ResourceID(self):
-        return self._cached_property('ResourceID', fake.uuid4)
-
-    @property
-    def ChargeType(self):
-        return self._cached_property('ChargeType', self.get_charge_type)
-    
-    @property
-    def Provider(self):
-        return self._cached_property('Provider', fake.company)
-    
-    @property
-    def BillingAccountName(self):
-        return self._cached_property('BillingAccountName', fake.company)
-    
-    @property
-    def SubAccountName(self):
-        # Only generate a new datetime when it's not in cache
-        if 'SubAccountName' not in self._cache:
-            # Generate a random datetime object within the last 30 days
-            random_datetime = fake.date_time_between(start_date=self.thirty_days_ago, end_date=self.now, tzinfo=pytz.utc)
-            formatted_date = datetime.strftime(random_datetime, '%Y-%m-%dT%H:%M:%SZ')
-            self._cache['SubAccountName'] = formatted_date
-
-        return self._cache['SubAccountName']
-
-    @property
-    def BillingAccountId(self):
-        return self._cached_property('BillingAccountId', fake.uuid4)
-
-    @property
-    def Publisher(self):
-        return self._cached_property('Publisher', self.get_aws_publisher)
-
-    @property
-    def ResourceName(self):
-        return self._cached_property('ResourceName', self.get_aws_resource_name)
-
-    @property
-    def ServiceName(self):
-        return self._cached_property('ServiceName', self.get_aws_service_name)
-
-    @property
-    def BilledCurrency(self):
-        return self._cached_property('BilledCurrency', lambda: 'USD')
-
-    @property
-    def BillingPeriodEnd(self):
-        # Only generate a new datetime when it's not in cache
-        if 'BillingPeriodEnd' not in self._cache:
-            # Generate a random datetime object within the last 30 days
-            random_datetime = fake.date_time_between(start_date=self.thirty_days_ago, end_date=self.now, tzinfo=pytz.utc)
-            formatted_date = datetime.strftime(random_datetime, '%Y-%m-%dT%H:%M:%SZ')
-            self._cache['BillingPeriodEnd'] = formatted_date
-
-        return self._cache['BillingPeriodEnd']
-
-    @property
-    def BillingPeriodStart(self):
-        # Only generate a new datetime when it's not in cache
-        if 'BillingPeriodStart' not in self._cache:
-            # Generate a random datetime object within the last 30 days
-            random_datetime = fake.date_time_between(start_date=self.thirty_days_ago, end_date=self.now, tzinfo=pytz.utc)
-            formatted_date = datetime.strftime(random_datetime, '%Y-%m-%dT%H:%M:%SZ')
-            self._cache['BillingPeriodStart'] = formatted_date
-
-        return self._cache['BillingPeriodStart']
-
-    @property
-    def Region(self):
-        return self._cached_property('Region', self.get_aws_region)
-
-    @property
-    def ServiceCategory(self):
-        return self._cached_property('ServiceCategory', self.get_aws_service_category)
-
-    @property
-    def ChargePeriodStart(self):
-        # Only generate a new datetime when it's not in cache
-        if 'ChargePeriodStart' not in self._cache:
-            # Generate a random datetime object within the last 30 days
-            random_datetime = fake.date_time_between(start_date=self.thirty_days_ago, end_date=self.now, tzinfo=pytz.utc)
-            formatted_date = datetime.strftime(random_datetime, '%Y-%m-%dT%H:%M:%SZ')
-            self._cache['ChargePeriodStart'] = formatted_date
-
-        return self._cache['ChargePeriodStart']
-
-    @property
-    def ChargePeriodEnd(self):
-        # Only generate a new datetime when it's not in cache
-        if 'ChargePeriodEnd' not in self._cache:
-            # Generate a random datetime object within the last 30 days
-            random_datetime = fake.date_time_between(start_date=self.thirty_days_ago, end_date=self.now, tzinfo=pytz.utc)
-            formatted_date = datetime.strftime(random_datetime, '%Y-%m-%dT%H:%M:%SZ')
-            self._cache['ChargePeriodEnd'] = formatted_date
-
-        return self._cache['ChargePeriodEnd']
-
-    @property
-    def BilledCost(self):
-        return self._cached_property('BilledCost', lambda: fake.pyfloat(left_digits=3, right_digits=2, positive=True))
-
-    @property
-    def AmortizedCost(self):
-        return self._cached_property('AmortizedCost', lambda: fake.pyfloat(left_digits=3, right_digits=2, positive=True))
-
-    def _cached_property(self, prop_name, generator_func):
-        if prop_name not in self._cache:
-            self._cache[prop_name] = generator_func()
-        return self._cache[prop_name]
-    
-    def to_dict(self):
-        return {
-            'InvoiceIssuer': self.InvoiceIssuer,
-            'ResourceID': self.ResourceID,
-            'ChargeType': self.ChargeType,
-            'Provider': self.Provider,
-            'BillingAccountName': self.BillingAccountName,
-            'SubAccountName': self.SubAccountName,
-            'BillingAccountId': self.BillingAccountId,
-            'Publisher': self.Publisher,
-            'ResourceName': self.ResourceName,
-            'ServiceName': self.ServiceName,
-            'BilledCurrency': self.BilledCurrency,
-            'BillingPeriodEnd': self.BillingPeriodEnd,
-            'BillingPeriodStart': self.BillingPeriodStart,
-            'Region': self.Region,
-            'ServiceCategory': self.ServiceCategory,
-            'ChargePeriodStart': self.ChargePeriodStart,
-            'ChargePeriodEnd': self.ChargePeriodEnd,
-            'BilledCost': self.BilledCost,
-            'AmortizedCost': self.AmortizedCost
-        }
-
-    
-    def get_aws_invoice_issuer(self):
-            aws_entities = [
-                'AWS Inc.', 'Amazon Web Services', 'AWS Marketplace', 
-                'Amazon Data Services', 'AWS CloudFront', 'Amazon S3 Billing', 
-                'Amazon EC2 Billing', 'AWS Lambda Billing'
-            ]
-            return str(random.choice(aws_entities))
-    
-    def get_charge_type(self):
-            aws_entities = [
-                'Adjustment', 'Purchase', 'Tax', 
-                'Usage'
-            ]
-            return str(random.choice(aws_entities))
-
-    def get_aws_publisher(self):
-            publisher_types = ['Software', 'Service', 'Platform']
-            publisher_suffix = random.choice(['Inc.', 'LLC', 'Ltd.', 'Group', 'Technologies', 'Solutions'])
-            return f"{fake.company()} {random.choice(publisher_types)} {publisher_suffix}"
-
-    def get_aws_resource_name(self):
-            resource_types = ['i-', 'vol-', 'snap-', 'ami-', 'bucket-', 'db-']
-            resource_prefix = random.choice(resource_types)
-            resource_id = fake.hexify(text='^^^^^^^^', upper=False)
-            return f'{resource_prefix}{resource_id}'
-
-    def get_aws_service_category(self):
-            aws_service_categories = [
-                'AI and Machine Learning', 'Analytics', 'Business Applications', 'Compute', 'Databases', 'Developer Tools', 'Multicloud',
-                'Identity', 'Integration', 'Internet of Things', 'Management and Governance', 'Media', 'Migration', 'Mobile', 'Networking',
-                'Security', 'Storage', 'Web', 'Other'
-            ]
-            return random.choice(aws_service_categories)
-
-    def get_aws_service_name(self):
-            aws_services = [
-                'Amazon EC2', 'Amazon S3', 'AWS Lambda', 'Amazon RDS', 
-                'Amazon DynamoDB', 'Amazon VPC', 'Amazon Route 53', 
-                'Amazon CloudFront', 'AWS Elastic Beanstalk', 'Amazon SNS', 
-                'Amazon SQS', 'Amazon Redshift', 'AWS CloudFormation', 
-                'AWS IAM', 'Amazon EBS', 'Amazon ECS', 'Amazon EKS', 
-                'Amazon ElastiCache', 'AWS Fargate', 'AWS Glue'
-            ]
-            return random.choice(aws_services)
-
-    def get_aws_region(self):
-            aws_regions = [
-                'us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-central-1',
-                'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-northeast-2',
-                'ap-south-1', 'sa-east-1', 'ca-central-1', 'eu-north-1', 'eu-west-2',
-                'eu-west-3', 'ap-east-1', 'me-south-1', 'af-south-1', 'eu-south-1'
-            ]
-            return random.choice(aws_regions)
-
-def generate_fake_focus():
-    return FakeFocus()
-
-def write_focus_to_csv(focus, csv_writer):
-    csv_writer.writerow(focus.to_dict())
 
 @log_execution_time
 def generate_and_write_fake_focuses(csv_filename, num_records):
-    headers = FakeFocus().to_dict().keys()
+    now = datetime.now(pytz.utc)
+    thirty_days_ago = now - timedelta(days=30)
 
-    with open(csv_filename, 'w', newline='') as csvfile:
-        csv_writer = csv.DictWriter(csvfile, fieldnames=headers)
-        csv_writer.writeheader()
+    df = pl.DataFrame({
+        'InvoiceIssuer': [random.choice([ 'AWS Inc.', 'Amazon Web Services', 'AWS Marketplace', 'Amazon Data Services', 
+                                         'AWS CloudFront', 'Amazon S3 Billing', 'Amazon EC2 Billing', 'AWS Lambda Billing']) for _ in range(num_records)],
+        'ResourceID': [fake.uuid4() for _ in range(num_records)],
+        'ChargeType': [random.choice(['Adjustment', 'Purchase', 'Tax', 'Usage']) for _ in range(num_records)],
+        'Provider': [fake.company() for _ in range(num_records)],
+        'BillingAccountName': [fake.company() for _ in range(num_records)],
+        'SubAccountName': get_random_datetimes(num_records, thirty_days_ago, now),
+        'BillingAccountId': [fake.uuid4() for _ in range(num_records)],
+        'Publisher': [f"{fake.company()} {random.choice(['Software', 'Service', 'Platform'])} {random.choice(['Inc.', 'LLC', 'Ltd.', 'Group', 'Technologies', 'Solutions'])}" for _ in range(num_records)],
+        'ResourceName': [f"{random.choice(['i-', 'vol-', 'snap-', 'ami-', 'bucket-', 'db-'])}{fake.hexify(text='^^^^^^^^', upper=False)}" for _ in range(num_records)],
+        'ServiceName': [random.choice([
+            'Amazon EC2', 'Amazon S3', 'AWS Lambda', 'Amazon RDS', 
+            'Amazon DynamoDB', 'Amazon VPC', 'Amazon Route 53', 
+            'Amazon CloudFront', 'AWS Elastic Beanstalk', 'Amazon SNS', 
+            'Amazon SQS', 'Amazon Redshift', 'AWS CloudFormation', 
+            'AWS IAM', 'Amazon EBS', 'Amazon ECS', 'Amazon EKS', 
+            'Amazon ElastiCache', 'AWS Fargate', 'AWS Glue'
+        ]) for _ in range(num_records)],
+        'BilledCurrency': ['USD' for _ in range(num_records)],
+        'BillingPeriodEnd': get_random_datetimes(num_records, thirty_days_ago, now),
+        'BillingPeriodStart': get_random_datetimes(num_records, thirty_days_ago, now),
+        'Region': [random.choice([
+            'us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-central-1',
+            'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-northeast-2',
+            'ap-south-1', 'sa-east-1', 'ca-central-1', 'eu-north-1', 'eu-west-2',
+            'eu-west-3', 'ap-east-1', 'me-south-1', 'af-south-1', 'eu-south-1'
+        ]) for _ in range(num_records)],
+        'ServiceCategory': [random.choice([
+            'AI and Machine Learning', 'Analytics', 'Business Applications', 'Compute', 'Databases', 'Developer Tools', 'Multicloud',
+            'Identity', 'Integration', 'Internet of Things', 'Management and Governance', 'Media', 'Migration', 'Mobile', 'Networking',
+            'Security', 'Storage', 'Web', 'Other'
+        ]) for _ in range(num_records)],
+        'ChargePeriodStart': get_random_datetimes(num_records, thirty_days_ago, now),
+        'ChargePeriodEnd': get_random_datetimes(num_records, thirty_days_ago, now),
+        'BilledCost': [fake.pyfloat(left_digits=3, right_digits=2, positive=True) for _ in range(num_records)],
+        'AmortizedCost': [fake.pyfloat(left_digits=3, right_digits=2, positive=True) for _ in range(num_records)]
+    })
 
-        with ThreadPoolExecutor() as executor:
-            futures = [executor.submit(generate_fake_focus) for _ in range(num_records)]
-            for future in futures:
-                focus = future.result()
-                write_focus_to_csv(focus, csv_writer)
+    df.write_csv(csv_filename)
---
+++ tests/test_performance_profiler.py
@@ -0,0 +1,107 @@
+import cProfile
+import csv
+import io
+import logging
+import os
+import pstats
+import time
+import unittest
+from ddt import ddt, data, unpack
+
+from tests.samples.csv_random_data_generate_at_scale import generate_and_write_fake_focuses
+from focus_validator.validator import Validator
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - [%(funcName)s] - %(message)s')
+
+@ddt
+class TestPerformanceProfiler(unittest.TestCase):
+    
+    def profile_to_csv(self, profiling_result, csv_file):
+        with open(csv_file, 'w', newline='') as f:
+            w = csv.writer(f)
+            # Write the headers
+            headers = ['ncalls', 'tottime', 'percall', 'cumtime', 'percall', 'filename:lineno(function)']
+            w.writerow(headers)
+        
+            # Write each row
+            for row in profiling_result.stats.items():
+                func_name, (cc, nc, tt, ct, callers) = row
+                w.writerow([nc, tt, tt/nc, ct, ct/cc, func_name])
+
+    def execute_profiler(self, file_name, performance_threshold):
+        # Set the environment variable for logging level
+        env = os.environ.copy()
+        env["LOG_LEVEL"] = "INFO"
+
+        # Get the current directory of this test file
+        test_dir = os.path.dirname(os.path.abspath(__file__))
+        base_dir =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        version_set_path=os.path.join(base_dir, "focus_validator", "rules", "version_sets")
+        validator = Validator(
+            data_filename=os.path.join(test_dir, '../' + file_name),
+            override_filename=None,
+            rule_set_path=version_set_path,
+            rules_version="0.5",
+            output_type="console",
+            output_destination=None,
+            column_namespace=None,
+        )
+
+        # Set up the profiler
+        profiler = cProfile.Profile()
+        profiler.enable()
+
+        # The original performance testing code
+        start_time = time.time()
+        validator.validate()
+        end_time = time.time()
+        duration = end_time - start_time
+        logging.info(f"File: {file_name} Duration: {duration} seconds")
+
+        # Stop the profiler
+        profiler.disable()
+
+        # Save profiling data to a file
+        profiling_result = pstats.Stats(profiler)
+        profile_file_name = "profiling_data_" + file_name
+        self.profile_to_csv(profiling_result, profile_file_name)
+
+        # Optionally print out profiling report to the console
+        s = io.StringIO()
+        sortby = 'cumulative'  # Can be changed to 'time', 'calls', etc.
+        ps = pstats.Stats(profiler, stream=s).sort_stats(sortby)
+        ps.print_stats(10)
+        logging.info(s.getvalue())
+
+        #Execution time check
+        self.assertLess(duration, performance_threshold, f"Performance test exceeded threshold. Duration: {duration} seconds")
+
+    @data(
+        # ("fake_focuses500000.csv", 60.0, 500000, "validate_500000_records"),
+        # ("fake_focuses250000.csv", 60.0, 250000, "validate_250000_records"),
+        # ("fake_focuses100000.csv", 30.0, 100000, "validate_100000_records"),
+        # ("fake_focuses50000.csv", 15.0, 50000, "validate_50000_records"),
+        # ("fake_focuses10000.csv", 7.0, 10000, "validate_10000_records"),
+        # ("fake_focuses5000.csv", 3.0, 5000, "validate_5000_records"),
+        ("fake_focuses2000.csv", 3.0, 2000, "validate_2000_records"),
+        ("fake_focuses2000.csv", 3.0, 1000, "validate_1000_records")
+    )
+    @unpack
+    def test_param_validator_performance(self, file_name, performance_threshold, number_of_records, case_id):
+        with self.subTest(case_id=case_id):
+            # Set the environment variable for logging level
+            env = os.environ.copy()
+            env["LOG_LEVEL"] = "INFO"
+
+            logging.info("Generating file with {number_of_records} records.")
+            generate_and_write_fake_focuses(file_name, number_of_records)
+            self.execute_profiler(str(file_name), performance_threshold)
+
+            logging.info("Cleaning up test file.")
+            base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            if os.path.exists(os.path.join(base_dir, file_name)):
+                os.remove(os.path.join(base_dir, file_name))
+    
+if __name__ == '__main__':
+    unittest.main()
