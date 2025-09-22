b5bae1a9b5fbad91927399ea013e2bc555bfacce
DanielRhee <danielrheewa@gmail.com>
2025-09-21 22:01:16 -0700

    implementation for the remainder of rules
    Signed-off-by: Mike Fuller <mike@finops.org>

--- focus_validator/config_objects/common.py
+++ focus_validator/config_objects/common.py
@@ -32,14 +32,32 @@ class SQLQueryCheck(BaseModel):
 
 
 class ValueComparisonCheck(BaseModel):
-    # Handles CheckNotValue, CheckValue, CheckGreaterOrEqualThanValue
-    operator: Literal["equals", "not_equals", "greater_equal"]
+    # Handles CheckNotValue, CheckValue, CheckGreaterOrEqualThanValue, CheckSameValue, CheckNotSameValue
+    operator: Literal["equals", "not_equals", "greater_equal", "not_equals_column", "equals_column"]
     value: Union[str, float, int, None]
 
 
 class FormatCheck(BaseModel):
     # Handles FormatNumeric, FormatDateTime, FormatBillingCurrencyCode, etc
-    format_type: Literal["numeric", "datetime", "currency_code", "string"]
+    format_type: Literal["numeric", "datetime", "currency_code", "string", "key_value", "unit"]
+
+
+class DistinctCountCheck(BaseModel):
+    # Handles CheckDistinctCount
+    column_a_name: str
+    column_b_name: str
+    expected_count: int
+
+
+class ConformanceRuleCheck(BaseModel):
+    # Handles CheckConformanceRule
+    conformance_rule_id: str
+
+
+class ColumnComparisonCheck(BaseModel):
+    # Handles ColumnByColumnEqualsColumnValue
+    comparison_column: str
+    operator: Literal["equals", "not_equals"]
 
 
 SIMPLE_CHECKS = Literal["check_unique", "column_required"]
